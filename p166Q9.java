e654312c53f85f2eaf7c44b7fa8fa348db6d3e6a
min040809 <93198769+min040809@users.noreply.github.com>
2021-11-04 21:48:28 +0900

    Create p166Q9.java

---
+++ p166Q9.java
@@ -0,0 +1,23 @@
+public class p166Q9 {
+
+	public static void main(String[] args) {
+		int intArray[][] = new int [4][];
+		
+		intArray[0] = new int[4];
+		intArray[1] = new int[4];
+		intArray[2] = new int[4];
+		intArray[3] = new int[4];
+		
+		
+		for(int i = 0; i<intArray.length; i++) {
+			for(int j = 0; j<intArray[i].length; j++)
+				intArray[i][j] = (int)(Math.random()*10 + 1);
+		}
+		
+		for(int i = 0; i<intArray.length; i++) {
+			for(int j = 0; j<intArray[i].length; j++)
+				System.out.print(intArray[i][j] + " ");
+			System.out.println();
+		}
+	}
+}
